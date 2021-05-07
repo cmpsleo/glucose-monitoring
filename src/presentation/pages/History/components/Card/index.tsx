ca003547a7bc37b1bce2b384509510817274a635
Leonardo Campos <cmpsleo3@gmail.com>
2021-05-07 00:25:02 -0300

    feat: add calendar component and history card

---
+++ src/presentation/pages/History/components/Card/index.tsx
@@ -0,0 +1,5 @@
+import * as S from "./styles";
+
+export function Card() {
+  return <S.Container>Card</S.Container>;
+}
--- src/presentation/pages/History/index.tsx
+++ src/presentation/pages/History/index.tsx
@@ -1,3 +1,24 @@
+import { Calendar } from "@/presentation/components";
+
+import { Card } from "./components";
+
 export function History() {
-  return <h1>Hello from History</h1>;
+  return (
+    <div>
+      <h1>Hello from History</h1>
+      <br />
+      <Calendar
+        calendars={[
+          {
+            date: new Date(),
+            items: [
+              {
+                content: <Card />,
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
 }
