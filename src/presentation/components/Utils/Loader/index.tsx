377e6a7099633019f0d33550a0e6fff856bdbcee
Leonardo Campos <cmpsleo3@gmail.com>
2021-05-09 18:01:34 -0300

    feat: add Loader component

---
+++ src/presentation/components/Utils/Loader/index.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Loader = styled.img.attrs({
+  src: "/icons/loader.svg",
+})``;
--- src/presentation/components/Utils/index.ts
+++ src/presentation/components/Utils/index.ts
@@ -1,2 +1,3 @@
+export * from "./Loader";
 export * from "./Container";
 export * from "./Conditional";
