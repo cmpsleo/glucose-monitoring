9350ac0e6f87d4045aa71e7a0a12794a308e52a8
Leonardo Campos <cmpsleo3@gmail.com>
2021-05-10 21:17:48 -0300

    chore: add coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,16 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  roots: ["<rootDir>/src"],
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/presentation/**",
+    "!src/**/stories.tsx",
+    "!src/pages/**/*.tsx",
+    "!src/main/**/*.ts",
+    "!types/*.d.ts",
+  ],
   moduleNameMapper: {
     "@/(.*)": "<rootDir>/src/$1",
   },
