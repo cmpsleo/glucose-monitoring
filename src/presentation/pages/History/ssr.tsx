a525049ba05b2537554247c651f2804a0bdec0fb
Leonardo Campos <cmpsleo3@gmail.com>
2021-05-10 22:02:09 -0300

    fix: lint warnings

--- src/presentation/pages/History/ssr.tsx
+++ src/presentation/pages/History/ssr.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback, useEffect } from "react";
+import { useState, useMemo } from "react";
 
 import { Measurement } from "@/domain/models";
 import { LoadHistory } from "@/domain/usecases";
